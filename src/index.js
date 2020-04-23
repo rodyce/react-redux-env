86e1a4e29cc4ab33212e2d9aa64f7bfdf281c4a9
Rodimiro Cerrato E <rodyce@gmail.com>
2020-04-22 21:11:13 -0600

    Instantiate store and provider
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Webpack will bundle this
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore(); // we may pass initial state into the store here.
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
