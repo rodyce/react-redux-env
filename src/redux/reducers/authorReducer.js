37da42b7f1e9e49e53d22b4ff8da83f47d94588f
Rodimiro Cerrato E <rodyce@gmail.com>
2020-04-27 21:01:21 -0600

    Set initial state and update reducers
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -1,6 +1,7 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function authorReducer(state = [], action) {
+export default function authorReducer(state = initialState.authors, action) {
   // Note. It would be better to store courses by ID in a map...
   // For faster lookups.
   switch (action.type) {
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,7 @@
 import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
 
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   // Note. It would be better to store courses by ID in a map...
   // For faster lookups.
   switch (action.type) {
