765d3aa21357a7c10177fac59378b4ba8e16f225
Rodimiro Cerrato E <rodyce@gmail.com>
2020-04-29 13:46:50 -0600

    Add course form with centralized change handler
    Signed-off-by: Rodimiro Cerrato E <rodyce@gmail.com>

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,26 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-class ManageCoursePage extends React.Component {
-  // constructor(props) {
-  //   super(props);
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  // Avoid using Redux for all state. Use plain React state for data
+  // only one few components use. (such as form state)
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
 
-  //   this.state = {
-  //     course: {
-  //       title: "",
-  //     },
-  //   };
-
-  //   Constructor binding.
-  //   Or use arrow function...
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
-
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+  useEffect(() => {
     if (courses.length === 0) {
       // Only load courses when list is empty.
       loadCourses().catch((error) => alert("Loading courses failed: " + error));
@@ -28,19 +26,37 @@ class ManageCoursePage extends React.Component {
     if (authors.length === 0) {
       loadAuthors().catch((error) => alert("Loading authors failed: " + error));
     }
+  }, []); // Note empty array here as a second argument.
+  // Effect will run once when the component mounts
+
+  function handleChange(event) {
+    // This destructure avoids the event getting garbage collected
+    // so that it is available within the nested setCourse callback.
+    const { name, value } = event.target;
+    // Use functional form of setState so I can safely set new state that
+    // is based on the existing state.
+    // Use computed property syntax. Ref property via variable.
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
   }
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+    />
+  );
 }
 
 // Adding this prop type avoid ES Lint to give a warning for props.dispatch
 ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
   loadAuthors: PropTypes.func.isRequired,
   loadCourses: PropTypes.func.isRequired,
 };
@@ -49,6 +65,7 @@ function mapStateToProps(state) {
   // Expose only what is necessary to avoid unneeded re-renders.
   // Add the author's name to each course.
   return {
+    course: newCourse,
     courses: state.courses,
     authors: state.authors,
   };
